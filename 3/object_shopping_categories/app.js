c27dde7d999d22344c45bffd4fe7734d465f79fd
sonercxt <soner.cxtdemir@gmail.com>
2022-01-06 11:42:55 +0100

    i have worked writing product-list on cards

--- 3/object_shopping_categories/app.js
+++ 3/object_shopping_categories/app.js
@@ -64,19 +64,35 @@ let electronics = [{
 
 //console.log(electronics[1].products[1])
 
-let electronicStuff = () => {
-   return document.getElementById("electronic-stuff").innerHTML = 
-    electronics.map((product,index)=>{
-     return ` <li class="list-group-item d-flex justify-content-between align-items-center" onclick="products(${index})">${product.categories}</li>`
-    }).join("")
-  
-    
+let electronicStuffAssign = () => {
+    return document.getElementById("electronic-stuff").innerHTML =
+        electronics.map((product, index) => {
+            return ` <li class="list-group-item d-flex justify-content-between align-items-center" onclick="productAssign()">${product.categories}
+     <span class="badge bg-primary rounded-pill">${index}</span></li>`
+        }).join("")
 }
 
 
-let products = (pIndex) =>{
-    
-} 
+let productAssign = () => {
+    //  let productProperty = electronics[pIndex].products;
+    return document.getElementById("product-list").innerHTML = electronics.map((product, index) => {
+        let productProperty = product.products[index];
+
+        return ` <div class="card" style="width: 12rem;">
+        <img src="https://cdn.britannica.com/w:400,h:300,c:crop/77/170477-050-1C747EE3/Laptop-computer.jpg"
+            class="card-img-top" alt="...">
+
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">${productProperty.productName}</li>
+            <li class="list-group-item">${productProperty.price}</li>
+            <li class="list-group-item">${productProperty.color}</li>
+        </ul>
+
+         </div>`
+    }).join("")
+
+}
+
 // let electronics = [{
 //     categories: "Computer",
 //     products: [{
